b890be63f3b0580a0f5259649062ae33dc81711a
eveningkid <arn.dellinger@gmail.com>
2018-11-27 18:24:10 +0100

    Initial Commit

---
+++ filmos/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  filmos
+//
+//  Created by Arnaud DELLINGER on 27/11/2018.
+//  Copyright © 2018 Arnaud DELLINGER. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ filmos/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  filmos
+//
+//  Created by Arnaud DELLINGER on 27/11/2018.
+//  Copyright © 2018 Arnaud DELLINGER. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
